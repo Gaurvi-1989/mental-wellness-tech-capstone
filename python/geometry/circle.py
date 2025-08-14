57d3dd9bb741992bb5e1189fe5750aa9cd1faaf7
Gaurvi Maheshwari <gaurvi0410@gmail.com>
2025-08-14 19:59:05 +0530

    Delete python directory

--- python/geometry/circle.py
+++
@@ -1,7 +0,0 @@
-def area_cir(radius):
-    area=3.14*(radius**2)
-    return area
-def circum(radius):
-    circumference=2*3.14*radius
-    return circumference 
-
--- python/geometry/rectangle.py
+++
@@ -1,7 +0,0 @@
-def area_rect(l,b):
-    area=l*b
-    return area
-
-def peri(l,b):
-    perimeter=2*(l+b)
-    return perimeter 
